06e8fae7fc724351c01239023769fb80b0f06bb8
Sudharshanbabufullstack <sudharshanbabu.fullstack@gmail.com>
2025-09-08 18:15:27 +0530

    Mouseactions in JS

---
+++ Javascript.js
@@ -0,0 +1,19 @@
+// script.js
+
+function showMessage() {
+    window.alert("You have clicked the button");
+}
+
+function shoemouseoverMessage() {
+    document.getElementById("output").textContent = "Yes, your cursor is on me. Now Click!";
+}
+
+function showMessage() {
+    window.alert("You have clicked the button!");
+    document.getElementById("output").textContent = "Button was clicked!";
+}
+
+console.log("Hello world");
+
+
+
